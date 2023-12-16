34b65f7356a16b8c16a4275717a686d638523645
ex8ed <an.proshak@gmail.com>
2023-12-16 04:47:43 +0300

    updates buttons and the keyboard

--- markups.py
+++ markups.py
@@ -1,9 +1,16 @@
 # -*- coding: UTF-8 -*-
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+# TODO: make objects with button namings
+btn_registry = KeyboardButton('👋 Регистрация')
+btn_get_message = KeyboardButton('📨 Мое задание')
+btn_get_help = KeyboardButton('🆘 Помощь')
+btn_get_tracker = KeyboardButton('🎫 Как там моя посылка?')
+btn_set_tracker = KeyboardButton('📦 Заявить трекер')
 
-btn_get_message = KeyboardButton('📨Мое задание')
-btn_get_tracker = KeyboardButton('🎫Как там моя посылка?')
-btn_get_help = KeyboardButton('🆘Помощь')
 
 main_menu = ReplyKeyboardMarkup(resize_keyboard=True)
-main_menu.add(btn_get_message, btn_get_tracker, btn_get_help)
+main_menu.add(btn_registry,
+              btn_get_message,
+              btn_get_help,
+              btn_get_tracker,
+              btn_set_tracker)
